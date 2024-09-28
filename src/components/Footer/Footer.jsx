8671ca31deb0872d003b4fbf6d052c5791fb5733
Deyler Mena <deyler_mena69222@elpoli.edu.co>
2024-09-27 21:22:43 -0500

    creacion de interfaz

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import "./Footer.css";
+
+/* Multi idioma */
+import { FormattedMessage } from "react-intl";
+
+const Footer = () => {
+  // Fecha
+  let fecha = new Date().getFullYear();
+
+  return (
+    <footer className="footer">
+      <div className="site-footer">
+        <div className="copyright">
+          <p>
+            <FormattedMessage
+              id="footer-info"
+              defaultMessage="Page created by Nahuel61920"
+            />
+          </p>
+          <p>&copy; {fecha}. All Rights Reserved.</p>
+        </div>
+        <div className="redes-sociales">
+          <a
+            href="https://www.linkedin.com/in/deyler-andres23/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className="fab fa-linkedin"></i>
+          </a>
+          <a
+            href="https://github.com/KeniBeck"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className="fab fa-github"></i>
+          </a>
+          <a
+            href="https://api.whatsapp.com/send?phone=5493815360966"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className="fab fa-whatsapp"></i>
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default React.memo(Footer);
